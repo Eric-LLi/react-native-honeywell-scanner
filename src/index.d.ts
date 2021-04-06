72ae2e82d4846c636756d6471f5bc4600859dc48
Eric-LLi <ericli1116@gmail.com>
2021-04-07 01:33:14 +1000

    <UPDATE>

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,12 +4,21 @@ export declare function stopReader(): Promise<null>;
 export declare function StartScan(): Promise<boolean>;
 export declare function StopScan(): Promise<boolean>;
 
-export declare function on(val: SCANNER_EVENTS, callback: (event: BarcodeTypes) => void): void;
+export declare function on(val: SCANNER_EVENTS, callback: Callbacks): void;
 export declare function off(val: SCANNER_EVENTS): void;
+
+export type Callbacks = onBarcodeResult | onStatusResult;
+
+type onBarcodeResult = (data: BarcodeTypes) => void;
+type onStatusResult = (data: StatusTypes) => void;
 
 export type BarcodeTypes = {
 	data: string;
 };
+
+export type StatusTypes = {
+	status: boolean
+}
 
 export enum SCANNER_EVENTS {
 	STATUS = 'BARCODE_STATUS',
